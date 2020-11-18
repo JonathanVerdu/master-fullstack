07830b68a09c3d2203567655843247854cec541e
Jonathan Verdu <gismou_88@hotmail.com>
2020-11-18 12:43:37 +0100

    19. Códigos de respuesta HTTP y respuestas en JSON

--- api-rest-laravel/app/Http/Controllers/UserController.php
+++ api-rest-laravel/app/Http/Controllers/UserController.php
@@ -11,9 +11,27 @@ class UserController extends Controller
    }
    
    public function register(Request $request){
-       $name = $request->input('name');
-       $surname = $request->input('surname');
-       return "Acción de registro de usuarios: $name $surname";
+       
+       // Recoger los datos del usuario por POST
+       
+       // Validar los datos
+       
+       // Cifrar la contraseña
+       
+       // Comprobar si el usuario existe ya (duplicado)
+       
+       // Crear el usuario
+       
+       // Devolver el mensaje correspondiente
+       
+        $data = array(
+            'status' => 'error',
+            'code' => 404,
+            'message' => 'El usuario no se ha creado'
+        );
+        
+        return response()->json($data, $data['code']);
+       
    }
    
    public function login(Request $request){
