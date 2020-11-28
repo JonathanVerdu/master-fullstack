757899d40f746aec3d0598a18f20555770955b15
Jonathan Verdu <gismou_88@hotmail.com>
2020-11-28 23:57:41 +0100

    40. Guardar un post

--- api-rest-laravel/app/Http/Controllers/PostController.php
+++ api-rest-laravel/app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Post;
+use App\Helpers\JwtAuth;
 
 class PostController extends Controller
 {
@@ -44,6 +45,61 @@ class PostController extends Controller
             
         }
         
+        return response()->json($data, $data['code']);
+    }
+    
+    public function store(Request $request){
+        // Recoger datos por Post
+        $json = $request->input('json', null);
+        $params = json_decode($json);
+        $params_array = json_decode($json, true);
+        
+        if(!empty($params_array)){
+            // Conseguir el usuario identificado
+            $jwtAuth = new JwtAuth;
+            $token = $request->header('Authorization', null);
+            $user = $jwtAuth->checkToken($token, true);
+
+            // Validar los datos
+            $validate = \Validator::make($params_array, [
+               'title' => 'required',
+               'content' => 'required',
+               'category_id' => 'required',
+               'image' => 'required'
+            ]);
+            
+            if($validate->fails()){
+                $data = [
+                    'code' => 400,
+                    'status' => "error",
+                    'message' => "Los datos no se han validado"
+                ];
+            }else{
+                // Guardar la entrada
+                $post = new Post();
+                $post->user_id = $user->sub;
+                $post->category_id = $params->category_id;
+                $post->title = $params->title;
+                $post->content = $params->content;
+                $post->image = $params->image;
+                $post->save();
+                
+                $data = [
+                    'code' => 200,
+                    'status' => "success",
+                    'post' => $post
+                ];
+            }
+
+        }else{
+            $data = [
+                'code' => 400,
+                'status' => "error",
+                'message' => "No has enviado datos"
+            ];
+        }
+        
+        // Devolver la respuesta
         return response()->json($data, $data['code']);
     }
      
